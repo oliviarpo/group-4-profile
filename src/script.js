392c73fab34aac637569f7b9a5f858602c3306d3
oliviarpo <oliviapovey@outlook.com>
2021-02-04 11:41:18 +0000

    Changed CSS

--- src/script.js
+++ src/script.js
@@ -1,10 +1,6 @@
 // Current date
 var dt = new Date();
 document.getElementById("datetime").innerHTML =
-  ("0" + dt.getHours()).slice(-2) +
-  ":" +
-  ("0" + dt.getMinutes()).slice(-2) +
-  ", " +
   ("0" + dt.getDate()).slice(-2) +
   "." +
   ("0" + (dt.getMonth() + 1)).slice(-2) +
